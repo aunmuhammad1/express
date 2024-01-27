986aa0e33c79fe52dda7310fbb9f04a0c1afaee1
Aun Muhammad <92050402+aunmuhammad6307@users.noreply.github.com>
2024-01-27 23:41:29 +0500

    set index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+// Use the PORT environment variable provided by Heroku,
+// or default to 3000 if running locally
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-    }
-);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
