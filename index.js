324c53edbac54eeb66bf49654eea16eff770dc96
Aun Muhammad <92050402+aunmuhammad6307@users.noreply.github.com>
2024-01-29 00:00:35 +0500

    add data to mongodb

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const port = 3000;
 
-// Use the PORT environment variable provided by Heroku,
-// or default to 3000 if running locally
-const port = process.env.PORT || 3000;
+const mongodburi = "mongodb+srv://daro-time:darotimepassword123@cluster0.1hcpvfd.mongodb.net/?retryWrites=true&w=majority"
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Connect to MongoDB
+mongoose.connect(mongodburi, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a schema for Todo
+const TodoSchema = new mongoose.Schema({
+    text: String
+});
+
+// Create a model
+const Todo = mongoose.model('Todo', TodoSchema);
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', async (req, res) => {
+    const todos = await Todo.find();
+    let todoList = todos.map(todo => `<li>${todo.text}</li>`).join('');
+    res.send(`
+        <h1>Todo List</h1>
+        <form action="/add-todo" method="post">
+            <input type="text" name="todo" placeholder="Add new todo">
+            <button type="submit">Add</button>
+        </form>
+        <ul>${todoList}</ul>
+    `);
+});
+
+app.post('/add-todo', (req, res) => {
+    const newTodo = new Todo({ text: req.body.todo });
+    newTodo.save().then(() => res.redirect('/'));
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
