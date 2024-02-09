881f1c15a69cdda7b8bb5350c8b4be6822ea2f4b
Aun Muhammad <92050402+aunmuhammad6307@users.noreply.github.com>
2024-02-09 23:17:17 +0500

    hello

--- sendmail.js
+++ sendmail.js
@@ -1,25 +1,26 @@
 const nodemailer = require('nodemailer');
 
 const sendmail = async (req, res) => {  
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'aubree.wilderman@ethereal.email',
-            pass: 'nPVUxQjTp6xF93FBRD'
-        }
-    });
+    res.send('Hello mail');
+    // const transporter = nodemailer.createTransport({
+    //     host: 'smtp.ethereal.email',
+    //     port: 587,
+    //     auth: {
+    //         user: 'aubree.wilderman@ethereal.email',
+    //         pass: 'nPVUxQjTp6xF93FBRD'
+    //     }
+    // });
 
-    const info = await transporter.sendMail({
-        from: "Aun Muhammad",
-        to: "aunmuhammad6307@gmail.com",
-        subject: "Hello",
-        text: "Hello world",
-        html: "<b>Hello world</b>"
-    })
+    // const info = await transporter.sendMail({
+    //     from: "Aun Muhammad",
+    //     to: "aunmuhammad6307@gmail.com",
+    //     subject: "Hello",
+    //     text: "Hello world",
+    //     html: "<b>Hello world</b>"
+    // })
 
-    console.log("Message sent: %s", info.messageId);
-    res.json(info);
+    // console.log("Message sent: %s", info.messageId);
+    // res.json(info);
 }
 
 module.exports = sendmail;
