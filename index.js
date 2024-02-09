fa75065a4bc95d360a67c614c336a6c41c319c4e
Aun Muhammad <92050402+aunmuhammad6307@users.noreply.github.com>
2024-02-09 23:19:07 +0500

    mail send

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const sendmail = require('./sendmail')
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
     }
 );
 
-app.get('sendmail', sendmail)
+app.get('sendmail', (req, res) => {
+    res.send('Mail sent!');
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
