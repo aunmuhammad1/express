ac3a4efb8609e8e91e8e2a4948b4a88901131cf3
Aun Muhammad <92050402+aunmuhammad6307@users.noreply.github.com>
2024-02-09 23:22:57 +0500

    sending

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const nodemailer = require('nodemailer');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -8,7 +9,32 @@ app.get('/', (req, res) => {
 );
 
 app.get('/sendmail', (req, res) => {
-    res.send('Mail sent!');
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'aubree.wilderman@ethereal.email',
+            pass: 'nPVUxQjTp6xF93FBRD'
+        }
+    });
+
+    const mailOptions = {
+        from: 'aun',
+        to: 'aunmuhammad6307@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!',
+        html: '<h1>Welcome</h1><p>That was easy!</p>'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+            res.send('Error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('Email sent: ' + info.response);
+        }
+    });
 })
 
 app.listen(port, () => {
